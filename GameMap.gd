5a2a5b19656a4533f61a82e8be23b58dd53e3cc7
Daniel Johnson <danieljohnson2@fastmail.com>
2018-07-15 15:34:10 -0400

    Beginning procedural map

--- GameMap.gd
+++ GameMap.gd
@@ -1,18 +1,25 @@
 extends Node2D
 onready var tankerTemplate = preload("res://Tanker.tscn")
-onready var segmentA = preload("res://root.tscn")
+onready var segmentA = preload("res://SegmentA.tscn")
+onready var segmentB = preload("res://SegmentB.tscn")
 
 func _ready():
-	var tileA = segmentA.instance()
-	var tileB = segmentA.instance()
 	var tankerA = tankerTemplate.instance()
 	var tankerB = tankerTemplate.instance()
+	var tile = []
 	
-	add_child(tileA)
-	add_child(tileB)
+	for i in range (0,260):
+		tile.append(segmentB)
+	
+	
+	for i in range (0,260):
+		var t = tile[i].instance()
+		add_child(t)
+		t.global_position = Vector2(0, i*-1000)
+	
+	
 	add_child(tankerA)
 	add_child(tankerB)
 	tankerB.control_key = KEY_B
 	tankerA.get_node(tankerA.tanker_camera).current = true
 	tankerB.get_node("Label").text = "B"
-	tileB.global_position = Vector2(0,-1000)
--- Tanker.gd
+++ Tanker.gd
@@ -12,8 +12,8 @@ func _ready():
 	pass
 
 func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
+	var directed_velocity = linear_velocity.rotated(-rotation)
+	
 	if get_node("Camera2D").current == true:
 		if (Input.is_action_pressed("ui_up")):
 			momentum_forward -= 10
@@ -29,13 +29,11 @@ func _process(delta):
 	if (Input.is_key_pressed(control_key)):
 		get_node("Camera2D").current = true
 	
-	var directed_velocity = linear_velocity.rotated(-rotation)
-	var turn_factor = directed_velocity.y * -0.02
 	directed_velocity.y = 0
 	directed_velocity.x *= -delta
 	
 	self.apply_impulse(Vector2 (0,0), directed_velocity.rotated(rotation))
 	self.apply_impulse(Vector2 (0,0), Vector2(0.0, momentum_forward*delta).rotated(rotation))
-	self.apply_impulse(Vector2 (0,10), Vector2 (momentum_turn*delta*turn_factor,0.0))
-	self.apply_impulse(Vector2 (0,-10), Vector2 (-momentum_turn*delta*turn_factor*0.5,0.0))
+	self.apply_impulse(Vector2 (0,10), Vector2 (momentum_turn*delta,0.0))
+	self.apply_impulse(Vector2 (0,-10), Vector2 (-momentum_turn*delta,0.0))
 
