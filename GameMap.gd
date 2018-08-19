09cbb3f1b9719122fef4b6cc489e8edebf2ea274
Chris Johnson <jinx6568@sover.net>
2018-08-19 17:31:44 -0400

    More tankers, control tweaks, expect some game segment tweaks

--- GameMap.gd
+++ GameMap.gd
@@ -4,6 +4,12 @@ onready var tankerTemplate = preload("res://Tanker.tscn")
 func _ready():
 	var tankerA = tankerTemplate.instance()
 	var tankerB = tankerTemplate.instance()
+	var tankerC = tankerTemplate.instance()
+	var tankerD = tankerTemplate.instance()
+	var tankerE = tankerTemplate.instance()
+	var tankerF = tankerTemplate.instance()
+	var tankerG = tankerTemplate.instance()
+	
 	var tile = []
 	var segments = [
 	preload("res://SegmentA.tscn"),
@@ -31,25 +37,53 @@ func _ready():
 	preload("res://SegmentW.tscn"),
 	preload("res://SegmentX.tscn"),
 	preload("res://SegmentY.tscn"),
-	preload("res://SegmentZ.tscn")]
+	preload("res://SegmentZ.tscn"),
+	preload("res://StartLine.tscn"),
+	preload("res://FinishLine.tscn")]
 	
-	for i in range (0,260):
-		var choice = 0
-		choice = (i*i*i)%int(floor(i/10)+1)
-		if i%10 == 0:
-			choice = floor(i/10)
-		
-		
-		tile.append(segments[choice])
+	var k = 0
+	tile.append(segments[26]) #StartLine
+	#print (26)
 	
-	for i in range (0,260):
+	for i in range (0,26):
+		for j in range (0, i+1):
+			var choice = 0
+			choice = (k*k)%int(floor(i)+1)
+			if (j == 0):
+				choice = i
+			tile.append(segments[choice])
+			#print (choice)
+			k += 1
+	tile.append(segments[27]) # FinishLine
+	#print (27)
+	
+	for i in range (0,353):
 		var t = tile[i].instance()
 		add_child(t)
 		t.global_position = Vector2(0, i*-1000)
 	
 	
 	add_child(tankerA)
-	#add_child(tankerB)
+	add_child(tankerB)
+	add_child(tankerC)
+	add_child(tankerD)
+	add_child(tankerE)
+	add_child(tankerF)
+	add_child(tankerG)
+	
 	tankerB.control_key = KEY_B
+	tankerC.control_key = KEY_C
+	tankerD.control_key = KEY_D
+	tankerE.control_key = KEY_E
+	tankerF.control_key = KEY_F
+	tankerG.control_key = KEY_G
+	
 	tankerA.get_node(tankerA.tanker_camera).current = true
 	tankerB.get_node("Label").text = "B"
+	tankerC.get_node("Label").text = "C"
+	tankerD.get_node("Label").text = "D"
+	tankerE.get_node("Label").text = "E"
+	tankerF.get_node("Label").text = "F"
+	tankerG.get_node("Label").text = "G"
+	
+	
