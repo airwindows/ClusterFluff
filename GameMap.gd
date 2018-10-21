c57af34e6a109f56be6d7ec12f15434272d2511c
Chris Johnson <jinx6568@sover.net>
2018-10-21 13:33:45 -0400

    Initial reducing to one screen

--- GameMap.gd
+++ GameMap.gd
@@ -10,57 +10,21 @@ func _ready():
 	var tankerF = tankerTemplate.instance()
 	var tankerG = tankerTemplate.instance()
 	
-	var tile = []
+	
 	var segments = [
-	preload("res://SegmentA.tscn"),
-	preload("res://SegmentB.tscn"),
-	preload("res://SegmentC.tscn"),
-	preload("res://SegmentD.tscn"),
-	preload("res://SegmentE.tscn"),
-	preload("res://SegmentF.tscn"),
-	preload("res://SegmentG.tscn"),
-	preload("res://SegmentH.tscn"),
-	preload("res://SegmentI.tscn"),
-	preload("res://SegmentJ.tscn"),
-	preload("res://SegmentK.tscn"),
-	preload("res://SegmentL.tscn"),
-	preload("res://SegmentM.tscn"),
-	preload("res://SegmentN.tscn"),
-	preload("res://SegmentO.tscn"),
-	preload("res://SegmentP.tscn"),
-	preload("res://SegmentQ.tscn"),
-	preload("res://SegmentR.tscn"),
-	preload("res://SegmentS.tscn"),
-	preload("res://SegmentT.tscn"),
-	preload("res://SegmentU.tscn"),
-	preload("res://SegmentV.tscn"),
-	preload("res://SegmentW.tscn"),
-	preload("res://SegmentX.tscn"),
-	preload("res://SegmentY.tscn"),
-	preload("res://SegmentZ.tscn"),
-	preload("res://StartLine.tscn"),
-	preload("res://FinishLine.tscn")]
+	"res://SegmentA.tscn",
+	"res://SegmentB.tscn",
+	"res://SegmentC.tscn",
+	"res://SegmentD.tscn",
+	"res://SegmentE.tscn",
+	"res://SegmentF.tscn",
+	"res://SegmentG.tscn",
+	"res://SegmentH.tscn"]
 	
-	var k = 0
-	tile.append(segments[26]) #StartLine
-	#print (26)
 	
-	for i in range (0,26):
-		for j in range (0, i+1):
-			var choice = 0
-			choice = (k*k)%int(floor(i)+1)
-			if (j == 0):
-				choice = i
-			tile.append(segments[choice])
-			#print (choice)
-			k += 1
-	tile.append(segments[27]) # FinishLine
-	#print (27)
-	
-	for i in range (0,350):
-		var t = tile[i].instance()
-		add_child(t)
-		t.global_position = Vector2(0, i*-1000)
+	var t = load(segments[0]).instance()
+	add_child(t)
+	t.global_position = Vector2(0, 0*-1000)
 	
 	
 	add_child(tankerA)
