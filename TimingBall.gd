864d8a3070e20ae90f0d9aed79732dccfa797252
Chris Johnson <jinx6568@sover.net>
2020-12-27 20:09:25 -0500

    Platform Binaries

--- TimingBall.gd
+++ TimingBall.gd
@@ -8,19 +8,19 @@ func _physics_process(delta):
 	cycle = !cycle
 	if cycle:
 		if self.linear_velocity.x > 0:
-			self.linear_velocity.x -= delta
+			self.linear_velocity.x -= (delta*2)
 		else:
-			self.linear_velocity.x += delta
+			self.linear_velocity.x += (delta*2)
 		$CollisionShape2D.scale.x = size
 		if (self.scale.x < 0.10):
 			self.queue_free()
 	else:
 		if self.linear_velocity.y > 0:
-			self.linear_velocity.y -= delta
+			self.linear_velocity.y -= (delta*2)
 		else:
-			self.linear_velocity.y += delta
+			self.linear_velocity.y += (delta*2)
 		$CollisionShape2D.scale.y = size
-		if (self.linear_velocity.length() < (150-(60*size))):
-			size -= ((1.0000001 - size)*delta)
+		if (self.linear_velocity.length() < (80-(60*size))):
+			size -= ((1.0000001 - size)*(delta*2))
 		else:
 			size = (size+1.0)*0.5
